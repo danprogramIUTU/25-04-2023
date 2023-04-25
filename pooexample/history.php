56ed3a321b6eccde9e5bc266d19970f9c1471d9f
Daniel Felipe <75703173+danprogramIUTU@users.noreply.github.com>
2023-04-25 15:10:18 -0500

    Add files via upload

---
+++ pooexample/history.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!--title and favicon-->
+    <title>|mechanic instantly</title>
+    <link rel="icon" type="image/png" href="./img/maintenance.png" />
+
+    <!--css bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+
+<body>
+
+
+    <!--navbar-->
+    <nav class="navbar navbar-expand-lg bg-secondary-subtle ">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+                aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="icon">
+                        <a class="iconhome" aria-current="page" href="#">
+                            <img src="img/maintenance.png" width="50px">
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="./home.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="#">Account your service</a>
+                    </li>
+                    <li class="nav-item">
+
+                    </li>
+                </ul>
+
+
+                <span class="navbar-text">
+                    <p class="fst-italic text-info">
+                        In every moment and every place
+                    </p>
+
+                </span>
+
+            </div>
+        </div>
+    </nav>
+
+
+    <div class="container-fluid align-middle text-center">
+        <div class="row">
+            <div class="col-12">
+                <p class="text-center text-warning ">We are a page dedicated to JDM history and show some of the most
+                    famous Japanese
+                    cars</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid ">
+        <div class="row vh-100 text-center align-center ">
+            <div class="row">
+                <div class="col-12">
+
+                    <div class="card" style="width: 18rem;">
+                        <img src="./img/civic.webp" class="card-img-top" alt="civic">
+                        <div class="card-body">
+                            <p class="card-text align-middle">The first generation of the Civic was launched on the
+                                market
+                                in
+                                July 1972 . Initially it had a two-door coupe body to which a three-door hatchback was
+                                added
+                                in
+                                September of this year. Later, the Hondamatic (with automatic transmission) and Civic
+                                CVCC
+                                were
+                                added in 1973.</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="card" style="width: 18rem;">
+                        <img src="./img/skyline.jpg" class="card-img-top" alt="gtr">
+                        <div class="card-body">
+                            <p class="card-text align-middle">The Nissan Skyline GT-R R34 was born in 1998 and
+                                is one of
+                                the
+                                most
+                                remembered for its participation in several renowned movies . The body took on a
+                                new,
+                                more
+                                aerodynamic design and had a shorter wheelbase. With that formula and, again,
+                                with the
+                                RB26DETT
+                                biturbo engine, it became a rival to beat.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="card " style="width: 18rem;">
+                        <img src="./img/supra.webp" class="card-img-top" alt="supra">
+                        <div class="card-body">
+                            <p class="card-text align-middle">The Supra actually took its first steps in
+                                the Japanese
+                                market
+                                as
+                                the
+                                Celica XX (Celica Supra in other markets) in 1978. It was a longer,
+                                wider and more
+                                powerful
+                                variant
+                                of the Celica, with a tuned six-cylinder engine and a more slender
+                                build. luxurious,
+                                perfect
+                                for
+                                the
+                                booming North American passenger car market.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="card" style="width: 18rem;">
+                        <img src="./img/rx-7.jpg" class="card-img-top" alt="rx-7">
+                        <div class="card-body">
+                            <p class="card-text align-middle">It was at the end of the 1970s,
+                                specifically in 1978, when
+                                Mazda
+                                debuted the RX - 7 : a sports car that distinguished itself by
+                                having a rotary engine,
+                                which
+                                contributed to the success of the Japanese brand in motorsports.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--java bootstrap-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ pooexample/home.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--title and favicon-->
+    <title>|mechanic instantly</title>
+    <link rel="icon" type="image/png" href="./img/maintenance.png" />
+
+    <!--css bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <!--carousel-->
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="./img/jdm.png" class="d-block w-100" alt="jdmcar">
+                <div class="carousel-caption d-none d-md-block">
+                    <div class="badge bg-primary text-wrap">
+                        <h4 class="text-red">At all times and in all places</h4>
+                        <h4 class="text-red">The best in repair, vehicle parts and customization</h4>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+
+        <!--navbar-->
+        <nav class="navbar navbar-expand-lg ">
+            <div class="container-fluid" >
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+                    aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="icon">
+                            <a class="iconhome" aria-current="page" href="#">
+                                <img src="img/maintenance.png" width="50px">
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="./history.php">History</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="#">Account your service</a>
+                        </li>
+                        <li class="nav-item">
+
+                        </li>
+                    </ul>
+
+                    
+                    <span class="navbar-text">
+                        <p class="fst-italic text-info">
+                            In every moment and every place
+                        </p>
+
+                    </span>
+
+                </div>
+            </div>
+        </nav>
+
+        <!--java bootstrap-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+            crossorigin="anonymous"></script>
+</body>
+
+</html>
